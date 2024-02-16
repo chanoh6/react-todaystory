3d9fc0ba91807a48f3a670c9a1ac2628f7c7a24c
hwakk <x_xhwakk@naver.com>
2024-02-16 18:56:02 +0900

    fix: index.js import 순서 변경

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import 'styles/index.css';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from 'App';
 import { Home, ContentDetail, Contents, NotFound } from 'pages';
 import 'locales/i18n';
-import 'styles/index.css';
 
 /**
  * @TODOS
