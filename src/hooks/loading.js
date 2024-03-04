dfc1f3fb3a3db148b254a5a30c9e7ed1eb66da76
kodawonKK <rhekdnjs0169@gmail.com>
2024-03-04 18:48:44 +0900

    fix: loading.js console.log 삭제

--- src/hooks/loading.js
+++ src/hooks/loading.js
@@ -4,12 +4,13 @@ import { useLocation } from 'react-router-dom';
 
 export const useLoading = () => {
   const [loading, setLoading] = useState(true);
+
   const location = useLocation();
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 1000);
+    }, 300);
   }, [location]);
-  console.log(location);
+
   return loading;
 };
